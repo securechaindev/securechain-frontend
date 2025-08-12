157c9c4156d9b765b5800d2691bb9096bbcfb2d1
GermanMT <germanoctako@gmail.com>
2025-08-12 13:17:27 +0200

    feat: Added nginx and next config for stactic frontend

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Use export for static files to serve with Nginx
   output: 'export',
-  // Disable features not compatible with static export
   trailingSlash: true,
   experimental: {
     ppr: false,

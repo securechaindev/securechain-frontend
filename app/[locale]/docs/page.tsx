84461f9adb6e162be5e7eccf56a4668030cc7d94
GermanMT <germanoctako@gmail.com>
2025-08-18 16:50:45 +0200

    fix: Fixed tabs triggers

--- app/[locale]/docs/page.tsx
+++ app/[locale]/docs/page.tsx
@@ -412,31 +412,37 @@ export default async function DocsPage({ params }: PageProps) {
           <Tabs defaultValue="overview" className="w-full">
             <TabsList className="grid w-full grid-cols-3 lg:grid-cols-6 h-auto">
               <TabsTrigger value="overview" className="text-xs sm:text-sm py-2 px-1 sm:px-3">
-                <span className="hidden sm:inline">{t.docs.overview}</span>
+                <span className="lg:hidden">{t.docs.overview}</span>
+                <span className="hidden lg:inline">{t.docs.overview}</span>
               </TabsTrigger>
               <TabsTrigger value="auth" className="text-xs sm:text-sm py-2 px-1 sm:px-3">
-                <span className="hidden sm:inline">{t.docs.auth}</span>
+                <span className="lg:hidden">{t.docs.auth}</span>
+                <span className="hidden lg:inline">{t.docs.auth}</span>
               </TabsTrigger>
               <TabsTrigger value="graph" className="text-xs sm:text-sm py-2 px-1 sm:px-3">
-                <span className="hidden sm:inline">{t.docs.graph}</span>
+                <span className="lg:hidden">{t.docs.graph}</span>
+                <span className="hidden lg:inline">{t.docs.graph}</span>
               </TabsTrigger>
               <TabsTrigger
                 value="file-ops"
-                className="text-xs sm:text-sm py-2 px-1 sm:px-3 col-span-3 lg:col-span-1"
+                className="text-xs sm:text-sm py-2 px-1 sm:px-3"
               >
+                <span className="lg:hidden">{t.docs.fileOps}</span>
                 <span className="hidden lg:inline">{t.docs.fileOps}</span>
               </TabsTrigger>
               <TabsTrigger
                 value="config-ops"
-                className="text-xs sm:text-sm py-2 px-1 sm:px-3 col-span-3 lg:col-span-1"
+                className="text-xs sm:text-sm py-2 px-1 sm:px-3"
               >
+                <span className="lg:hidden">{t.docs.configOps}</span>
                 <span className="hidden lg:inline">{t.docs.configOps}</span>
               </TabsTrigger>
               <TabsTrigger
                 value="schemas"
-                className="text-xs sm:text-sm py-2 px-1 sm:px-3 col-span-3 lg:col-span-1"
+                className="text-xs sm:text-sm py-2 px-1 sm:px-3"
               >
-                <span className="hidden sm:inline">{t.docs.schemas}</span>
+                <span className="lg:hidden">{t.docs.schemas}</span>
+                <span className="hidden lg:inline">{t.docs.schemas}</span>
               </TabsTrigger>
             </TabsList>
 
